4e9d9b97dc7cd4e2eee7ef03a799ef9c332feab5
Илья <@WIN-RANNDDD>
2026-05-25 17:25:54 +0400

    Spring Boot project initializion

---
+++ exam-backend/src/test/java/com/github/randdd32/exam_backend/ExamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.randdd32.exam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
